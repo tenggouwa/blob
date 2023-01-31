2f001538fd81b1a45eb0faaa0151ee241dd90f71
tenggouwa <17681875268@163.com>
2023-02-01 01:29:09 +0800

    feat: add babel arco

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,6 +1,29 @@
-import popSortFun from './pages/home';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { Button, Card, Link } from "@arco-design/web-react";
+import "@arco-design/web-react/dist/css/arco.css";
 
 
+const AppPage = () => {
+  const [count, setCount] = useState(0);
+  
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1) }>{count}</button>
+      <Button type="primary" onClick={() => setCount(count + 1) }>{count}</Button>
+      <Card style={{ width: 360 }}
+        title='Arco Card'
+        extra={<Link>More</Link>}
+      >
+        ByteDance's core product, Toutiao ('Headlines'), is a content platform in China and around
+        the world. Toutiao started out as a news recommendation engine and gradually evolved into a
+        platform delivering content in various formats.
+      </Card>
+    </div>
+  )
+}
 
-const arr = [2, 9, 7, 1, 4, 3, 8];
-console.log(popSortFun(arr));
+ReactDOM.render(
+  <AppPage />,
+  document.getElementById('root')
+);
